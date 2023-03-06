ac480a9f6850197ada5f4e71eb7317e3bb93ea91
cheogn97 <cheogn97@gmail.com>
2023-03-06 17:41:13 +0900

    [최정헌] @Slf4j 로깅 세팅

--- src/main/java/goodee/e1i6/movie/MovieApplication.java
+++ src/main/java/goodee/e1i6/movie/MovieApplication.java
@@ -2,10 +2,12 @@ package goodee.e1i6.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class MovieApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MovieApplication.class, args);
 	}
---
+++ src/main/java/goodee/e1i6/movie/teamColor/TeamColor.java
@@ -0,0 +1,4 @@
+package goodee.e1i6.movie.teamColor;
+public class TeamColor {
+	public static final String CHOI = "\u001B[31m"; // 최정헌
+}
