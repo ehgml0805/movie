7162b7733225d8aa883171422df00e033f9dc226
YoungwooJ <cyw960714@naver.com>
2023-03-07 11:45:31 +0900

    [조영우] @slf4j 디버깅 로그 개개인 색상 부여

--- src/main/java/goodee/e1i6/movie/teamColor/TeamColor.java
+++ src/main/java/goodee/e1i6/movie/teamColor/TeamColor.java
@@ -1,4 +1,10 @@
 package goodee.e1i6.movie.teamColor;
 public class TeamColor {
+	public static final String KDH = "\u001B[36m"; // 김도희
+	public static final String KSH = "\u001B[33m"; // 김소현
+	public static final String YIB = "\u001B[37m"; // 양인범
+	public static final String LJW = "\u001B[34m"; // 이지원
+	public static final String JYW = "\u001B[35m"; // 조영우
+	public static final String JSM = "\u001B[32m"; // 정성민
 	public static final String CHOI = "\u001B[31m"; // 최정헌
 }
