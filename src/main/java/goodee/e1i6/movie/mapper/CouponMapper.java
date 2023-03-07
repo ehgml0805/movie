ad0e82ee2931204491b1d3e463b2720395c48c6a
dorang <ehgml0805@naver.com>
2023-03-07 14:51:02 +0900

    [김도희]coupon mapper 생성 (ing)

---
+++ src/main/java/goodee/e1i6/movie/mapper/CouponMapper.java
@@ -0,0 +1,21 @@
+package goodee.e1i6.movie.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import goodee.e1i6.movie.vo.Coupon;
+
+@Mapper
+public interface CouponMapper {
+	
+		//쿠폰삭제(처음 잘못 등록시에만 삭제 가능)
+		int deleteCoupon(int couponKey);
+		//쿠폰 발행(등록)
+		int insertCoupon(Coupon coupon);
+		//쿠폰 상세보기
+		Coupon selectCouponOne(int couponKey);
+		//모든 쿠폰 리스트 출력
+		List<Coupon> selectAllCouponList(Map<String, Object> paramMap);
+}
