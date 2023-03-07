0758f8cd60adb22e10a78b444fc48c5a2504b451
cheogn97 <cheogn97@gmail.com>
2023-03-07 16:26:09 +0900

    [최정헌, 조영우] 웹 크롤링(JSoup)을 통한 영화 DB 저장 및 출력 기능 구현

--- src/main/java/goodee/e1i6/movie/controller/HomeController.java
+++ src/main/java/goodee/e1i6/movie/controller/HomeController.java
@@ -1,5 +1,7 @@
 package goodee.e1i6.movie.controller;
 
+import java.util.Iterator;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,6 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 	@GetMapping("/homeEx")
 	public String getHome() {
+		
+			String[] arr = {"a","b","c","d","e"};
+			
+			for(int i = 0; i < arr.length; i++) {
+				for(int j = 1; j < 13; j++) {
+					String seatNumber = arr[i] + (j);			
+					System.out.println(seatNumber);					
+				}
+			}
+		
 		return "homeEx";
 	}
 }
