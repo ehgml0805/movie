467d85f2588b09675d3828724af32efa4e4f323f
cheogn97 <cheogn97@gmail.com>
2023-03-08 13:15:49 +0900

    [최정헌] 주소 API VO 생성, RestController 패키지명 변경

---
+++ src/main/java/goodee/e1i6/movie/vo/Postcode.java
@@ -0,0 +1,12 @@
+package goodee.e1i6.movie.vo;
+
+import lombok.Data;
+
+@Data
+public class Postcode {
+	private String postcode;
+	private String roadAddress;
+	private String jibunAddress;
+	private String detailAddress;
+	private String extraAddress;
+}
