3d39b3b28d1092d5538e20cacd419ded3ea16e6f
YoungwooJ <cyw960714@naver.com>
2023-03-14 14:58:11 +0900

    [조영우] 인코딩, 고객 로그인 시 필터 생성

--- src/main/java/goodee/e1i6/movie/MovieApplication.java
+++ src/main/java/goodee/e1i6/movie/MovieApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
-@ServletComponentScan
+@ServletComponentScan /* 웹필터(애노테이션) 사용 가능하도록 */
 @SpringBootApplication
 public class MovieApplication {
 	
---
+++ src/main/java/goodee/e1i6/movie/filter/CustomerLoginFilter.java
@@ -0,0 +1,44 @@
+package goodee.e1i6.movie.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import goodee.e1i6.movie.teamColor.TeamColor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j // static log log = new log();
+@WebFilter("/customer/*")
+public class CustomerLoginFilter implements Filter {
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		log.debug(TeamColor.JYW+"EmpLoginFilter");
+		
+		if(request instanceof HttpServletRequest) {
+			HttpServletRequest req = (HttpServletRequest)request;
+			HttpSession session = ((HttpServletRequest) request).getSession();
+			
+			if(session.getAttribute("loginCustomer") == null) {
+				((HttpServletResponse)response).sendRedirect(req.getContextPath()+"/loginCustomer");
+				return;
+			}
+		} else {
+			log.debug("웹브라우저 요청만 허용합니다.");
+			return;
+		}
+		
+		// controller 전
+		chain.doFilter(request, response);
+		// controller 후
+	}
+}
