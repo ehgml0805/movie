0440df749cfe965c5af89281803f6b316faeedb8
dorang <ehgml0805@naver.com>
2023-03-15 17:33:03 +0900

    [김도희] 리뷰 CRUD + 신고리스트 구현 중

---
+++ src/main/java/goodee/e1i6/movie/mapper/ReviewMapper.java
@@ -0,0 +1,26 @@
+package goodee.e1i6.movie.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+import goodee.e1i6.movie.vo.Review;
+
+public interface ReviewMapper {
+	
+	
+	//스포일러 신고 업데이트
+	int spoilerReport(String customerId);
+	//욕설, 비방 신고 업데이트
+	int insultReport(String customerId);
+	
+	//리뷰삭제?
+	int deleteReview(int ticketing_key, String customerId);
+	//리뷰수정
+	int updateReview(Review review);
+	//리뷰 작성
+	int insertReview(Review review);
+	//리뷰 페이징 총 개수
+	int selectCount();
+	//리뷰 리스트 출력
+	List<Review> selectReviewList(Map<String, Object> paramMap);
+}
