4e3afc400885b9b4fc09e7b37d10b44f6fa14653
dlsqja3561 <dlsqja3561@naver.com>
2023-03-21 14:00:01 +0900

    [양인범] 에러 수정

--- src/main/java/goodee/e1i6/movie/restController/QuestionRestController.java
+++ src/main/java/goodee/e1i6/movie/restController/QuestionRestController.java
@@ -7,21 +7,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import goodee.e1i6.movie.service.TheaterService;
+import goodee.e1i6.movie.service.QuestionService;
 import goodee.e1i6.movie.teamColor.TeamColor;
 import goodee.e1i6.movie.vo.Theater;
 import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 public class QuestionRestController {
-	@Autowired TheaterService theaterService;
+	@Autowired QuestionService questionService;
 	
 	// 지역선택 후 극장 리스트 받기
 	@GetMapping("/question/theaterList")
 	public List<Theater> getTheaterListByRegion(@RequestParam(value="theaterRegion") String theaterRegion) {
 		log.debug(TeamColor.YIB + "get 극장 리스트--");
 		log.debug(TeamColor.YIB + theaterRegion + " : theaterRegion");
-		return theaterService.geTheaterNameListByRegion(theaterRegion);
+		return questionService.getTheaterNameListByRegion(theaterRegion);
 
 	}
 }
