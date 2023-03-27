7ff6ec0b5b40fd907edd1f765421ed1776c7a795
YoungwooJ <cyw960714@naver.com>
2023-03-27 17:16:45 +0900

    [조영우] 카카오페이 API - 결제 준비할 때 데이터 값 보내기

--- src/main/java/goodee/e1i6/movie/restController/KakaoRestController.java
+++ src/main/java/goodee/e1i6/movie/restController/KakaoRestController.java
@@ -3,6 +3,8 @@ package goodee.e1i6.movie.restController;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 import goodee.e1i6.movie.service.KaKaoService;
 import goodee.e1i6.movie.teamColor.TeamColor;
+import goodee.e1i6.movie.vo.Customer;
 import goodee.e1i6.movie.vo.KakaopayReady;
 import goodee.e1i6.movie.vo.Ticketing;
 import lombok.extern.slf4j.Slf4j;
@@ -25,34 +28,41 @@ public class KakaoRestController {
 	
 	// 카카오페이 결제 요청
     @GetMapping("/kakaopay")
-    public KakaopayReady readyToKakaoPay(Model model
+    public KakaopayReady readyToKakaoPay(Model model, HttpSession session
 											, @RequestParam(value = "scheduleKey", required = true) int scheduleKey
 											, @RequestParam(value = "mycouponKey", required = true) int mycouponKey
-											, @RequestParam(value = "customerId", required = true) String customerId
 											, @RequestParam(value = "totalPrice", required = true) int totalPrice
 											, @RequestParam(value = "discountPrice", required = true) int discountPrice
 											, @RequestParam(value = "kakaoPrice", required = true) int kakaoPrice
-											, @RequestParam(value = "seatKey", required = true) int[] seatKey
+											, @RequestParam(value = "seatKey", required = true) String[] seatKey
 											, @RequestParam(value = "movieTitle", required = true) String movieTitle) {
     	log.debug(TeamColor.JYW + "GET kakaoPay");
     	
+    	Customer customer = (Customer)session.getAttribute("loginCustomer");
     	Ticketing ticketing = new Ticketing();
     	ticketing.setScheduleKey(scheduleKey);
     	ticketing.setMycouponKey(mycouponKey);
-    	ticketing.setCustomerId(customerId);
+    	ticketing.setCustomerId(customer.getCustomerId());
     	ticketing.setTotalPrice(totalPrice);
     	ticketing.setDiscountPrice(discountPrice);
     	
     	// ticketing정보를 모델에 저장
     	model.addAttribute("ticketing",ticketing);
-    	log.debug(TeamColor.JYW + "ticketing");
+    	log.debug(TeamColor.JYW + "ticketing :" + ticketing);
+    	
+    	// seatKey정보를 모델에 저장
+    	model.addAttribute("seatKey",seatKey);
+    	log.debug(TeamColor.JYW + "seatKey : " + seatKey);
     	
     	Map<String, Object> paramMap = new HashMap<String, Object>();
-    	paramMap.put("customerId", customerId);
+    	paramMap.put("customerId", customer.getCustomerId());
+    	log.debug(TeamColor.JYW + "customer.getCustomerId() : " + customer.getCustomerId());
     	paramMap.put("movieTitle", movieTitle);
+    	log.debug(TeamColor.JYW + "movieTitle : " + movieTitle);
     	paramMap.put("quantity", seatKey.length);
+    	log.debug(TeamColor.JYW + "seatKey.length : " + seatKey.length);
     	paramMap.put("kakaoPrice", kakaoPrice);
-    	paramMap.put("movieTitle", movieTitle);
+    	log.debug(TeamColor.JYW + "kakaoPrice : " + kakaoPrice);
     	
     	
         return kaKaoService.kakaopayReady(paramMap);
